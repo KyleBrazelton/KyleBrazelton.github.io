6ef64e0d8e892b46e48c3d94a3c8b99c4a8d3c80
kylebrazelton <kylejbrazelton@gmail.com>
2019-06-01 01:09:02 -0600

    week6

---
+++ js/lab6-1.js
@@ -0,0 +1,25 @@
+// Get Current Date
+let today = new Date(); // new Date object
+// now concatenate formatted output
+let date = (today.getMonth() + 1) + " / " + today.getDate() + " / " + today.getFullYear();
+document.getElementById('currentdate').innerHTML = date;
+
+//INPUT get weather input - hot rain snow cold
+//PROCESSING match the weather to type of footwear appropriate
+//OUTPUT show the proper footwear for the weather
+function weatherFootFunction() {
+    let winput = document.getElementById("winput").value;
+    let footWear;
+
+    if (winput == "hot") {
+        footWear = "Sandals";
+    } else if (winput == "rain") {
+        footWear = "Galoshes";
+    } else if (winput == "snow") {
+        footWear = "Boots";
+    } else if (winput == "cool") {
+        footWear = "Shoes";
+    }
+
+    document.getElementById("foot").innerHTML = footWear;
+}
---
+++ js/lab6-2.js
@@ -0,0 +1,29 @@
+// Get Current Date
+let today = new Date(); // new Date object
+// now concatenate formatted output
+let date = (today.getMonth() + 1) + " / " + today.getDate() + " / " + today.getFullYear();
+document.getElementById('currentdate').innerHTML = date;
+
+
+
+// fast way to calculate a random number and display it for the child to work out the problem
+
+let random1 = Math.floor(Math.random() * (100 - 1)) + 1;
+let random2 = Math.floor(Math.random() * (100 - 1)) + 1;
+
+let equation = document.getElementById('equation').innerHTML = random1 + "+" + random2;
+
+function checkSolution() {
+    //INPUT get two positive integers max 100/addition/
+    let solution = document.getElementById('solution').value;
+    let feedBack;
+    //PROCESSING get integers, check if true compare integers
+    let answer = random1 + random2;
+    if (solution == answer) {
+        feedBack = "Correct! Good job!"
+    } else {
+        feedBack = "Sorry. That is incorrect."
+    }
+    //OUTPUT answer, of the two integers
+    document.getElementById('output').innerHTML = feedBack;
+}
