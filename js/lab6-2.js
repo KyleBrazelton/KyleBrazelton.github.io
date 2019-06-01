44ae6d87460981bccb1bd57c599446146affd059
kylebrazelton <kylejbrazelton@gmail.com>
2019-06-01 01:22:52 -0600

    week6/4

--- js/lab6-2.js
+++ js/lab6-2.js
@@ -7,25 +7,28 @@ document.getElementById('currentdate').innerHTML = date;
 
 
 // fast way to calculate a random number and display it for the child to work out the problem
-function getRando() {
+
+let random1;
+let random2;
+
+function generateProblem() {
     let random1 = Math.floor(Math.random() * (100 - 1)) + 1;
     let random2 = Math.floor(Math.random() * (100 - 1)) + 1;
-
     document.getElementById('equation').innerHTML = random1 + "+" + random2;
+}
 
+function checkSolution() {
 
-    function checkSolution() {
-        //INPUT get two positive integers max 100/addition/
-        let solution = document.getElementById('solution').value;
-        let feedBack;
-        //PROCESSING get integers, check if true compare integers
-        let answer = random1 + random2;
-        if (solution == answer) {
-            feedBack = "Correct! Good job!"
-        } else {
-            feedBack = "Sorry. That is incorrect."
-        }
-        //OUTPUT answer, of the two integers
-        document.getElementById('output').innerHTML = feedBack;
+    //INPUT get two positive integers max 100/addition/
+    let solution = document.getElementById('solution').value;
+    let feedBack;
+    //PROCESSING get integers, check if true compare integers
+    let answer = random1 + random2;
+    if (solution == answer) {
+        feedBack = "Correct! Good job!"
+    } else {
+        feedBack = "Sorry. That is incorrect."
     }
+    //OUTPUT answer, of the two integers
+    document.getElementById('output').innerHTML = feedBack;
 }
