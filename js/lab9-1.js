612b602efd1fd4dfbf19ac39a6299270c0e8d33f
kylebrazelton <kylejbrazelton@gmail.com>
2019-06-21 20:02:06 -0600

    week9

---
+++ js/lab9-1.js
@@ -0,0 +1,25 @@
+// Get Current Date
+let today = new Date(); // new Date object
+// now concatenate formatted output
+let date =
+    today.getMonth() + 1 + " / " + today.getDate() + " / " + today.getFullYear();
+document.getElementById("currentdate").innerHTML = date;
+
+//INPUT get user input from any integers 1-10 inclusive
+//PROCESSING take number and apply it to a times table for that number
+//OUTPUT times table for user input for user to see
+
+function getTable() {
+    let userPick = parseInt(document.getElementById("userPick").value);
+    if (userPick > 10 || userPick < 1) {
+        alert("Please choose a number between 1-10");
+        return;
+    }
+
+    let table = "";
+    for (let i = 1; i <= 12; i++) {
+        let result = i * userPick;
+        table += userPick + " x " + i + " = " + result + "<br>";
+    }
+    document.getElementById("output").innerHTML = table;
+}
---
+++ js/lab9-2.js
@@ -0,0 +1,22 @@
+// Get Current Date
+let today = new Date(); // new Date object
+// now concatenate formatted output
+let date =
+    today.getMonth() + 1 + " / " + today.getDate() + " / " + today.getFullYear();
+document.getElementById("currentdate").innerHTML = date;
+
+//INPUT get input from HTML list, on oldest temple.
+//PROCESSING use loop to process html temple data
+//OUTPUT  display temple data
+let table = document.getElementById("templeData");
+let year = parseInt(table.rows[1].cells[1].innerHTML);
+let rows = document.getElementById("templeData").rows.length;
+for (let i = 1; i <= rows; i++) {
+    let dYear = parseInt(table.rows[i].cells[1].innerHTML);
+    if (dYear < year) {
+        year = parseInt(table.rows[i].cells[1].innerHTML);
+        let oldest = table.rows[i].cells[0].innerHTML;
+        document.getElementById("output").innerHTML =
+            "The Oldest Running Temple is " + oldest;
+    }
+}
