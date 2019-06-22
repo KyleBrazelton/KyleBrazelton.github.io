cc8e0c24931daf846228f9d83fc65475e37245ca
kylebrazelton <kylejbrazelton@gmail.com>
2019-06-21 20:29:38 -0600

    Week9-jsfix

--- js/lab9-2.js
+++ js/lab9-2.js
@@ -8,15 +8,21 @@ document.getElementById("currentdate").innerHTML = date;
 //INPUT get input from HTML list, on oldest temple.
 //PROCESSING use loop to process html temple data
 //OUTPUT  display temple data
-let table = document.getElementById("templeData");
-let year = parseInt(table.rows[1].cells[1].innerHTML);
-let rows = document.getElementById("templeData").rows.length;
-for (let i = 1; i <= rows; i++) {
-    let dYear = parseInt(table.rows[i].cells[1].innerHTML);
-    if (dYear < year) {
-        year = parseInt(table.rows[i].cells[1].innerHTML);
-        let oldest = table.rows[i].cells[0].innerHTML;
-        document.getElementById("output").innerHTML =
-            "The Oldest Running Temple is " + oldest;
+function templeTable() {
+    let table = document.getElementById("templeData");
+    let year = parseInt(table.rows[1].cells[1].innerHTML);
+    let rows = document.getElementById("templeData").rows.length;
+
+    for (let i = 1; i <= table.rows.length; i++) {
+        let dYear = parseInt(table.rows[i].cells[1].innerHTML);
+        if (dYear < year) {
+            year = parseInt(table.rows[i].cells[1].innerHTML);
+            let oldest = table.rows[i].cells[0].innerHTML;
+            document.getElementById("output").innerHTML =
+                "The Oldest Running Temple is " + oldest;
+        }
     }
+}
+window.onload = function() {
+    templeTable();
 }
