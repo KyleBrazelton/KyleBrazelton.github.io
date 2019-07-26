52c63555bfd6d69d2ea6a1f4fe27a1d3cec668a8
kylebrazelton <kylejbrazelton@gmail.com>
2019-07-26 14:29:38 -0600

    change format

---
+++ RPSLS/js/currentdate.js
@@ -0,0 +1,5 @@
+window.onload = function curDate() {
+    let today = new Date();
+    let date = (today.getMonth() + 1) + " / " + today.getDate() + " / " + today.getFullYear();
+    document.getElementById('currentdate').innerHTML = date;
+}
