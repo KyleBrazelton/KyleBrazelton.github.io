f470205bec54fdbcdd39a686a4894051e96fbec0
kylebrazelton <kylejbrazelton@gmail.com>
2020-02-03 16:33:54 -0700

    week5initial

---
+++ cit230/lesson5/js/datemod.js
@@ -0,0 +1,30 @@
+window.onload = function getDate() {
+    let date = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    var month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+    var curMonth = month[date.getMonth()];
+
+    var curDay = weekday[date.getDay()];
+    let strDate = curDay + ", " + date.getDate() + " " + curMonth + " " + date.getFullYear();
+    document.getElementById("currentDate").innerHTML = strDate;
+}
